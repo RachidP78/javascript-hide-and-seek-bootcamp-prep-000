294fb3222461e273c7a21cdf1308cd89bbc938e8
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-31 12:28:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  // accepts a selector and returns the first element that matches.
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  var increaseRank = document.getElementById('app').querySelectorAll('u1.ranked-list')
+  for (let i = 0; i < increaseRank.length; i++) {
+    increaseRank[i].innerHTML = ((parseInt(increaseRank[i].innerHTML)) + n)
+  }
+}
+
+function deepestChild() {
+
+}
