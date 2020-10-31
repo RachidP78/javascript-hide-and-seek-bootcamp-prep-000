f1e245b9b0af9bbdc9a0801e40965a3fc85fb19a
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-31 12:52:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+function getFirstSelector(selector) {
+  // accepts a selector and returns the first element that matches.
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML)) + n
+  }
+}
+
+function deepestChild() {
+  let gNode = document.querySelector('#grand-node')
+  let nextNode = gNode.children[0]
+
+  while (nextNode) {
+    gNode = nextNode
+    nextNode = gNode.children[0]
+  }
+  return (gNode)
+}
